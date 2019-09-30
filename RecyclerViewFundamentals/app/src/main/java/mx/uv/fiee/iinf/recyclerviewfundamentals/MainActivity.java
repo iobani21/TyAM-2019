ba410c80d8c60c4630af59a3be9be13c38220bb9
Gonzalo Antonio Sosa Malaga <gonzasosa@outlook.com>
2019-09-30 17:04:25 -0500

    Recyclerview Fundamentals

--- RecyclerViewFundamentals/app/src/main/java/mx/uv/fiee/iinf/recyclerviewfundamentals/MainActivity.java
+++ RecyclerViewFundamentals/app/src/main/java/mx/uv/fiee/iinf/recyclerviewfundamentals/MainActivity.java
@@ -1,9 +1,17 @@
 package mx.uv.fiee.iinf.recyclerviewfundamentals;
 
+import android.content.Context;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -25,11 +33,60 @@ public class MainActivity extends AppCompatActivity {
         planets.add ("Urano");
         planets.add ("Neptuno");
 
-        ArrayAdapter<String> myAdapter =new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, planets);
-
         RecyclerView recyclerView = findViewById (R.id.myList);
-        recyclerView.setAdapter (myAdapter);
+        recyclerView.setLayoutManager (new LinearLayoutManager (this, RecyclerView.VERTICAL, false));
+        recyclerView.setAdapter (new MyAdapter (this, planets));
     }
 }
+
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+    private Context context;
+    private ArrayList<String> planets;
+
+    MyAdapter (Context context, ArrayList<String> planets) {
+        this.context = context;
+        this.planets = planets;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from (context);
+        View view = inflater.inflate (android.R.layout.simple_list_item_1, parent, false);
+        return new MyViewHolder (view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        final String planet = planets.get (position);
+        holder.setData (planet);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText (context, planet, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return planets.size ();
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        private TextView text1;
+
+        MyViewHolder(@NonNull View itemView) {
+            super (itemView);
+            text1 = itemView.findViewById (android.R.id.text1);
+        }
+
+        void setData (String data) {
+            text1.setText (data);
+        }
+    }
+}
+
 
 
